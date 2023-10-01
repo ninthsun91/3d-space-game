acfea5f74618ba2b710dd329665cacf68c90c372
ninthsun91 <ninthsun91@gmail.com>
2023-10-02 04:23:44 +0900

    :heavy_plus_sign: Install dependencies - threejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import {} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Perf }  from 'r3f-perf';
 
 function App() {
 
   return (
     <>
+      <Canvas>
+        <Perf debug />
+      </Canvas>
     </>
   )
 }
