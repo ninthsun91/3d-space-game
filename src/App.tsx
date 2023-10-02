1d9cba1641e238610220c9beed41cfa1d8216714
ninthsun91 <ninthsun91@gmail.com>
2023-10-03 00:55:42 +0900

    :recycle: Split Game component from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,11 @@
 import './App.css'
 import {} from 'react'
-import { Canvas } from '@react-three/fiber'
-import { KeyboardControls } from '@react-three/drei';
-import { Physics } from '@react-three/rapier';
-import { Perf }  from 'r3f-perf';
-
-import { Lane, Lights, Player, Rocks, Stars, Walls } from './three';
+import { Game } from './Game';
 
 function App() {
 
   return (
-    <KeyboardControls
-      map={[
-        { name: 'forward', keys: [ 'KeyW', 'ArrowUp' ] },
-        { name: 'backward', keys: [ 'KeyS', 'ArrowDown' ] },
-        { name: 'left', keys: [ 'KeyA', 'ArrowLeft' ] },
-        { name: 'right', keys: [ 'KeyD', 'ArrowRight' ] },
-      ]}
-    >
-      <Canvas
-        camera={{
-          fov: 105,
-          near: 0.1,
-          far: 10000,
-        }}
-      >
-        <Perf debug />
-        <Lights />
-
-        <Stars />
-        <Lane />
-
-        <Physics
-          debug
-          gravity={[ 0, 0, 0 ]}
-        >
-          <Walls />
-
-          <Player />
-          <Rocks />
-        </Physics>
-      </Canvas>
-    </KeyboardControls>
+    <Game />
   )
 }
 
